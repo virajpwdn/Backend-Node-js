6cf4eb56aae63d53020cfe91ee3e4737c1657e2c
virajpwdn <virajpatwardhan6@gmail.com>
2025-02-19 03:13:11 +0530

    completed login route with UI

--- fullstackinsta/Frontend/Frontend/src/views/Profile/Profile.jsx
+++ fullstackinsta/Frontend/Frontend/src/views/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import './profilestyle.css'
 
 const Profile = () => {
   const [data, setData] = useState({});
@@ -9,17 +10,27 @@ const Profile = () => {
         headers: { Authorization: "bearer " + localStorage.getItem("token") },
       })
       .then((response) => {
-          setData(response.data);
+        setData(response.data);
       })
       .catch((error) => {
         console.log(error.response.data.error);
       });
   }, []);
 
-  return <div>
-    {/* {console.log(data.message.username)} */}
-    <h1>{data.message?.username}</h1>
-  </div>;
+  return (
+    <>
+      <div className="container">
+        <div className="profile-img"><img src={data.message?.photo || "https://images.unsplash.com/photo-1555066931-4365d14bab8c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29kaW5nfGVufDB8fDB8fHww"} alt="" /></div>
+        <h2 className="username">{data.message?.username}</h2>
+        <div className="post-container">
+          <div className="post"></div>
+          <div className="post"></div>
+          <div className="post"></div>
+          <div className="post"></div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Profile;
--- fullstackinsta/Frontend/Frontend/src/views/login/Login.jsx
+++ fullstackinsta/Frontend/Frontend/src/views/login/Login.jsx
@@ -1,18 +1,67 @@
-import React from 'react'
-import './loginstyle.css'
+import React, { useState } from "react";
+import "./loginstyle.css";
+import axios from "axios";
+import { useNavigate } from "react-router";
 
 const Login = () => {
-  return (
-    <div className='login-container'>
-        <form>
-            <label htmlFor="email">Email</label>
-            <input id='email' type="email" placeholder='Enter your email'/>
-            <label htmlFor="password">Password</label>
-            <input id='password' type="password" placeholder='Enter your password'/>
-            <button type='submit'>Login</button>
-        </form>
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-export default Login
+  const navigate = useNavigate();
+
+  const loginHandler = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/user/login", {
+        email,
+        password,
+      })
+      .then((response) => {
+        setError("");
+        localStorage.setItem("token", response.data.token);
+        navigate("/profile");
+        console.log("You are successfully logged in");
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+        setError(error.response.data.message);
+      });
+
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={loginHandler}>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          required
+        />
+        <button className="login-submit-btn" type="submit">
+          Login
+        </button>
+        {error && <p>{error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default Login;
