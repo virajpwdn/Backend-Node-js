859d0d96e37e54f1c88dd2f2e1090b9480cbc60e
virajpwdn <virajpatwardhan6@gmail.com>
2025-02-20 17:19:13 +0530

    bug fixed: populate error

--- fullstackinsta/Frontend/Frontend/src/views/Profile/Profile.jsx
+++ fullstackinsta/Frontend/Frontend/src/views/Profile/Profile.jsx
@@ -25,10 +25,12 @@ const Profile = () => {
   return (
     <>
       <div className="container">
-        <div className="profile-img"><img src={data.message?.photo || "https://images.unsplash.com/photo-1555066931-4365d14bab8c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29kaW5nfGVufDB8fDB8fHww"} alt="" /></div>
-        <h2 className="username">{data.message?.username}</h2>
+        <div className="profile-img"><img src={data?.user?.photo || "https://images.unsplash.com/photo-1555066931-4365d14bab8c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29kaW5nfGVufDB8fDB8fHww"} alt="" /></div>
+        <h2 className="username">{data?.user?.username}</h2>
         <div className="post-container">
-          <div className="post"></div>
+          <div className="post">
+            <img src={data?.user?.posts.media} alt="" />
+          </div>
           <div className="post"></div>
           <div className="post"></div>
           <div className="post"></div>
