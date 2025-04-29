26c54f3fcdc74fd44664e83f41c69aa13a4bcbc7
virajpwdn <virajpatwardhan6@gmail.com>
2025-04-29 23:44:52 +0530

    created a image to contanarized

--- socialmedia/server.js
+++ socialmedia/server.js
@@ -10,11 +10,11 @@ const {connectDB} = require("./src/database/db")
 connectDB.then(()=>{
     try {
         console.log("Connected to database");
-        app.listen("3000", (req,res)=>{
+        app.listen("3000", ()=>{
             console.log("Connected to server");
         })
     } catch (error) {
-        console.log("ERROR:" + ERROR.message);
+        console.log("ERROR:" + error.message);
     }
 }
 )
